fffb48b7d7cb96282160e6239232c04a5fc75150
hr_sabbir <hanifurrahmansabbir@gmail.com>
2018-05-06 03:30:24 +0600

    fixed bug

--- app/Structure.php
+++ app/Structure.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Structure extends Model
 {
-    protected $table = 'ESCTRUCTURAS';
+    protected $table = 'ESTRUCTURAS';
     protected $primaryKey = 'ID';
     public $timestamps = false;
     protected $fillable = [
